321cba85c212bf97bf75b0e37c9b35638b9d4991
Z1mo0x <droweckiy@mail.ru>
2023-02-07 21:37:16 +0300

    header__burger

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 const priceAttemtion = document.querySelector('.price__attemption');
+const burger = document.querySelector('.header__burger');
+const list = document.querySelector('.header__list');
 
 priceAttemtion.addEventListener('click', () => {
     priceAttemtion.classList.add('deleted')
 });
+
+burger.addEventListener('click', () => {
+    list.classList.toggle('active')
+});
+
