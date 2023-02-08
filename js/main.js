ab667c8b823299a454c0f041a716efc6aa122b2d
Z1mo0x <droweckiy@mail.ru>
2023-02-08 19:23:29 +0300

    burger__menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 const priceAttemtion = document.querySelector('.price__attemption');
 const burger = document.querySelector('.header__burger');
 const list = document.querySelector('.header__list');
+const html = document.documentElement;
+const body = document.body;
 
 priceAttemtion.addEventListener('click', () => {
     priceAttemtion.classList.add('deleted')
 });
 
 burger.addEventListener('click', () => {
-    list.classList.toggle('active')
-});
+    html.classList.toggle('scroll__off');
+    body.classList.toggle('scroll__off');
+    html.scrollTo(0, 0)
+    html.scrollBy(0, 0)
+    burger.classList.toggle('active_burger');
+    list.classList.toggle('active');
+})
 
